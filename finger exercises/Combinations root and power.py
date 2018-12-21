250574ecf6325fa7d490edf2cf64dc67929daaf3
shreykuntal <45984673+shreykuntal@users.noreply.github.com>
2018-12-21 17:17:00 +0530

    Update Combinations root and power.py

--- finger exercises/Combinations root and power.py
+++ finger exercises/Combinations root and power.py
@@ -1,34 +1,26 @@
 print("ATTENTION: CHOOSE NUMBERS ABOVE 1!")
-a=int(input("Enter an integer: "))
-pwr=1
-root=1
-b=0
+a = int(input("Enter an integer: "))
+pwr = 1
+root = 1
+b = 0
 while True:
-    
-    if root**pwr!=a:
-        pwr+=1
-        if pwr==6:
-            pwr-=5
-            root+=1
-        if root**pwr==a:
+    if root ** pwr != a:
+        pwr += 1
+        if pwr == 6:
+            pwr -= 5
+            root += 1
+        if root ** pwr == a:
             print(str(root)+'^'+str(pwr)+'='+str(a))
-            b+=1
-            root+=1
-        
-        if root**2<=a:
+            b += 1
+            root += 1
+        if root ** 2 <= a:
             continue
-        if root**2>a:
+        if root ** 2 > a:
             break
-    if root**1==a:
+    if root ** 1 == a:
         print(str(root)+'^'+str(1)+'='+str(a))
-            
-    
-       
-    
-    
-if b<1:
+if b < 1:
     print("No combinations available")
-        
 else: 
     print("No more combinations available")
         
