857ec752f0277188696908c90784928ca906f23f
shreykuntal <45984673+shreykuntal@users.noreply.github.com>
2019-01-23 20:11:10 +0530

    Add files via upload

---
+++ finger exercises/debt_clearing.py
@@ -0,0 +1,17 @@
+def debt(balance, annualInterestRate):
+    monthly_payment = 10
+    monthly_interest_rate = annualInterestRate/12.0
+    init_balance = balance
+    while True:
+        for i in range(12):
+            monthly_unpaid_balance = balance - monthly_payment
+            balance = monthly_unpaid_balance + (monthly_interest_rate * monthly_unpaid_balance)
+            print('Unpaid Balance =', monthly_unpaid_balance)
+            print('MIR * MUB =',monthly_interest_rate * monthly_unpaid_balance)
+        if balance <= 0:
+            print('Lowest payment:', monthly_payment)
+            break
+        else:
+            monthly_payment += 10
+            balance = init_balance
+print(debt(3329,.2))
